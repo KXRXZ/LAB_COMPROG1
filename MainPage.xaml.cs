08b116312a5443b24511f1272d37fcb66669e285
Comlab <Comlab@CMLB2-26>
2023-10-09 08:32:41 +0800

    Add project files.

---
+++ MainPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿namespace LAB_COMPROG1;
+
+public partial class MainPage : ContentPage
+{
+	int count = 0;
+
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+	async void OnDial(object sender, EventArgs e)
+	{
+		try
+		{
+			PhoneDialer.Open(PhoneNum.Text);
+			Convert.ToInt32(PhoneNum.Text);	
+		}
+		catch
+		{
+			await DisplayAlert("Invalid Input", "Please enter a valid phone number", "OK");
+		}
+	}
+	
+}
+
+
